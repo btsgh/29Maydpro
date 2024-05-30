b3bf8f20e5f111326dfb8ceb0a0f97d733995cf4
Aparna Ramachandran <aparna@appsmith.com>
2024-05-30 08:17:07 +0000

    commit

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,17 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		
+		var x = _.add(341,353)
+		return x
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFun2 () {
+		console.log (moment.duration(50,'seconds'))
+		
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
